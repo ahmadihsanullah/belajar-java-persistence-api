cd295014cf6156da5bd8d3ae0c6732ac70971ada
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-03-18 13:33:13 +0700

    add: entity

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE belajar_java_peristence_api;
+
+USE belajar_java_persistence_api;
+
+CREATE Table customers(
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
